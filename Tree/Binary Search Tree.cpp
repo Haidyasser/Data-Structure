ec8a8f104e77aecfcb9d9fd9d98194e425e22d5f
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2022-12-22 17:41:14 +0200

    Create Binary Search Tree.cpp

---
+++ Tree/Binary Search Tree.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+class BST{
+private:
+    int data;
+    BST* left, *right;
+public:
+    BST(int _data = 0){
+        data = _data;
+        left = right = NULL;
+    }
+    bool isEmpty();
+    int Length(BST* root);
+    void printInorder(BST* root);
+    void printPreorder(BST* root);
+    void printPostorder(BST* root);
+    int getDepth(BST* root);
+    BST* Insert(BST*root, int value);
+    BST* Delete(BST* root, int value);
+    bool findNode(BST* root, int value);
+    BST* getMin(BST* root);
+    BST* getMax(BST* root);
+    int getData(BST* root);
+};
+
+bool BST::isEmpty(){
+    return this == NULL;
+}
+// count size of binary tree
+int BST::Length(BST *root) {
+    if(!root)
+        return 0;
+    return 1 + Length(root -> left) + Length(root -> right);
+}
+void BST::printInorder(BST *root) {
+    if(!root)
+        return;
+    printInorder(root -> left);
+    cout << root ->data << " ";
+    printInorder(root ->right);
+}
+void BST::printPreorder(BST *root) {
+    if(!root)
+        return;
+    cout << root ->data << " ";
+    printPreorder(root -> left);
+    printPreorder(root -> right);
+}
+void BST::printPostorder(BST *root) {
+    if(!root)
+        return;
+    printPostorder(root -> right);
+    printPostorder(root -> left);
+    cout << root ->data << " ";
+}
+int BST::getDepth(BST *root) {
+    if(!root)
+        return 0;
+    return 1 + max(getDepth(root -> right), getDepth(root -> left));
+}
+BST* BST::Insert(BST* root, int value) {
+    if(!root) {
+        return new BST(value);
+    }
+    if(root -> data > value)
+        root -> left = Insert(root -> left, value);
+    else
+        root -> right = Insert(root -> right, value);
+    return root;
+}
+
+BST* BST::getMin(BST* root) {
+    if(!root)
+        return root;
+    if(!root -> left)
+        return root;
+    return getMin(root -> left);
+}
+BST* BST::getMax(BST* root) {
+    if(!root)
+        return root;
+    if(!root -> right)
+        return root;
+    return getMax(root -> right);
+}
+
+BST* BST::Delete(BST *root, int value) {
+    if(!root)
+        return root;
+    else if(root -> data > value)
+        root -> left = Delete(root -> left, value);
+    else if(root -> data < value)
+        root -> right = Delete(root -> right, value);
+    else{
+        if(root -> left == NULL && root -> right == NULL){
+            delete root;
+            root = NULL;
+        }
+        else if(root -> left == NULL){
+            BST* temp = root;
+            root = root -> right;
+            delete temp;
+        }
+        else if(root -> right == NULL){
+            BST* temp = root;
+            root = root -> left;
+            delete temp;
+        }
+        else{
+            BST* temp = getMin(root -> right);
+            root -> data = temp -> data;
+            Delete(root->right, temp->data);
+        }
+    }
+    return root;
+}
+bool BST::findNode(BST* root, int value) {
+    if(!root)
+        return 0;
+    if(root -> data == value)
+        return 1;
+    if(root -> data > value)
+        return findNode(root -> left, value);
+    else
+        return findNode(root -> right, value);
+}
+int BST::getData(BST *root) {
+    return root -> data;
+}
+int main()
+{
+    freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+    BST *tree = NULL, obj;
+    tree = obj.Insert(tree, 10);
+    obj.Insert(tree, 8);
+    obj.Insert(tree, 7);
+    obj.Insert(tree, 9);
+    obj.printPostorder(tree);
+    cout << '\n';
+    obj.printPreorder(tree);
+    cout << '\n';
+    obj.printInorder(tree);
+    cout << '\n';
+    cout << obj.getDepth(tree) << endl;
+    cout << obj.Length(tree) << endl;
+    cout << obj.findNode(tree, 7) << endl;
+    cout << obj.findNode(tree, 11) << endl;
+    cout << obj.getData(obj.getMin(tree)) << endl;
+    cout << obj.getData(obj.getMax(tree)) << endl;
+    obj.Delete(tree, 10);
+    obj.printInorder(tree);
+    return 0;
+}
